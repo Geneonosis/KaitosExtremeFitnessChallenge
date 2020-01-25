581ca811cf294bb1ac947778f6794b8300ad2e59
JadaE <jadaebong@gmail.com>
2020-01-25 17:28:41 -0600

    initial push

---
+++ Assets/MenuScripts/LogoLoading.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoLoading : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(1); // 0 = loading, 1 = log in
+
+    }
+}
