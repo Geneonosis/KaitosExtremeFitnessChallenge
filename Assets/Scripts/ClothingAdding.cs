15e17fcc683c445c65604702447059538e29fc8a
Kaito <rars213@gmail.com>
2020-01-26 09:55:52 -0600

    stuuff
    ljva;sl

--- Assets/Scripts/ClothingAdding.cs
+++ Assets/Scripts/ClothingAdding.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ClothingAdding : MonoBehaviour
 {
     public SpriteRenderer part;
-    public Sprite[] head;
-    public Sprite[] clothes;
-    public Sprite[] bodycolor;
-   
+    public Sprite[] options;
+    public Image thisImage;
+    //public Sprite[] clothes;
+    //public Sprite[] bodycolor;
 
+    GameObject itemType;
+
+    int index;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +23,20 @@ public class ClothingAdding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < options.Length; i++)
+        {
+            part.sprite = options[i];
+        }
     }
-    public void swap()
+    public void Swap()
     {
-
+        if(index < options.Length - 1)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
     }
 }
