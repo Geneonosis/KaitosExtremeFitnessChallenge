872ba16f38026f4cc71dc66816e084e16b31163b
JadaE <jadaebong@gmail.com>
2020-01-26 08:56:36 -0600

    merp

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void playButtonClick()
+    {
+        SceneManager.LoadScene(4);//4 = play menu
+    }
+    public void settingsButtonClick()
+    {
+        SceneManager.LoadScene(5);//5 = settings
+    }
+}
