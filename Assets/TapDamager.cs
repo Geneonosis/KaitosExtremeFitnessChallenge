55fa9a482cd752a9b44071912a5e0fdc4ec1e8b0
geneonosis <genetigner.art@gmail.com>
2020-01-26 05:38:58 -0600

    ADDED scripts and tapping mechanic
    added scripts and tapping mechanics

---
+++ Assets/TapDamager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapDamager : MonoBehaviour
+{
+    int damage;
+    [SerializeField] GameObject boss = null;
+    [SerializeField] AnimationClip damageBossAnimation = null;
+    public void tapDamage()
+    {
+        boss.GetComponent<Animation>().clip = damageBossAnimation;
+        boss.GetComponent<Animation>().Play();
+        Debug.Log("apply damage");
+    }
+}
