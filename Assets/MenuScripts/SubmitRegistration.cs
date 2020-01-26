277de84e69254744f49b45760356f61c4ffe4fcb
JadaE <jadaebong@gmail.com>
2020-01-25 18:24:31 -0600

    Register Scene button works
    Still need to save the data to a database

---
+++ Assets/MenuScripts/SubmitRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubmitRegistration : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void submitButtonClick()
+    {
+        SceneManager.LoadScene(1);//1 = log in
+    }
+}
