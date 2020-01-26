427c367b948a530a83693a61d849232b2e22aaa7
JadaE <jadaebong@gmail.com>
2020-01-25 19:09:58 -0600

    Log in and reg update and connects
    yep

--- Assets/MenuScripts/SubmitRegistration.cs
+++ Assets/MenuScripts/SubmitRegistration.cs
@@ -5,17 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SubmitRegistration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
     public void submitButtonClick()
     {
         SceneManager.LoadScene(1);//1 = log in
