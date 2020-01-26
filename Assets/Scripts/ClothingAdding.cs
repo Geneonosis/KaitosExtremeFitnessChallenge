c4d326dd69355dad18106667fbd4169e49711f1a
Kaito <rars213@gmail.com>
2020-01-26 11:49:24 -0600

    Home final draft
    got costumiztions working

--- Assets/Scripts/ClothingAdding.cs
+++ Assets/Scripts/ClothingAdding.cs
@@ -1,42 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class ClothingAdding : MonoBehaviour
 {
-    public SpriteRenderer part;
-    public Sprite[] options;
-    public Image thisImage;
+    public Sprite image;
+    public GameObject Head;
+    public GameObject Shirt;
+    public GameObject Weapon;
+    public GameObject Soccer;
+    public GameObject FullBody;
+
     //public Sprite[] clothes;
     //public Sprite[] bodycolor;
 
-    GameObject itemType;
+    //GameObject itemType;
 
     int index;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < options.Length; i++)
-        {
-            part.sprite = options[i];
-        }
+
     }
-    public void Swap()
+    
+   
+    public void Change()
     {
-        if(index < options.Length - 1)
+        if (tag.Equals("Head"))
         {
-            index++;
+            if (Head.GetComponent<Image>().enabled != true)
+            {
+                Head.GetComponent<Image>().enabled = true;
+
+            }
+            Head.GetComponent<Image>().overrideSprite = image;
+            
+
+        }
+        else if (tag.Equals("Shirt"))
+        {
+            if (Shirt.GetComponent<Image>().enabled != true)
+            {
+                Shirt.GetComponent<Image>().enabled = true;
+            }
+            Shirt.GetComponent<Image>().overrideSprite = image;
+            
+        }
+        else if (tag.Equals("Weapon"))
+        {
+            if (Weapon.GetComponent<Image>().enabled != true)
+            {
+                Weapon.GetComponent<Image>().enabled = true;
+            }
+            if(Soccer.GetComponent<Image>().enabled == true)
+            {
+                Soccer.GetComponent<Image>().enabled = false;
+            }
+            Weapon.GetComponent<Image>().overrideSprite = image;
+        }
+        else if (tag.Equals("Soccer"))
+        {
+            if (Soccer.GetComponent<Image>().enabled != true)
+            {
+                Soccer.GetComponent<Image>().enabled = true;
+            }
+            if (Weapon.GetComponent<Image>().enabled == true)
+            {
+                Weapon.GetComponent<Image>().enabled = false;
+            }
+            Soccer.GetComponent<Image>().overrideSprite = image;
+        }
+        else if (tag.Equals("Avatar"))
+        {
+            if (FullBody.GetComponent<Image>().enabled != true)
+            {
+                FullBody.GetComponent<Image>().enabled = true;
+            }
+            FullBody.GetComponent<Image>().overrideSprite = image;
         }
         else
         {
-            index = 0;
+            print("system fail");
         }
     }
 }
