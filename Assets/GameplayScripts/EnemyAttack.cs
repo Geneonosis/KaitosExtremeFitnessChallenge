d06c566f63287afac2efdc93a27b18294eab6f9e
JadaE <jadaebong@gmail.com>
2020-01-26 02:51:18 -0600

    health stuff

---
+++ Assets/GameplayScripts/EnemyAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetAttacks = .5f;
+    public int attackDamage = 10;
+    GameObject player;
+    PlayerHealth ph;
+    EnemyHealth eh;
+    float timer;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        ph = player.GetComponent<PlayerHealth>();
+        eh = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
